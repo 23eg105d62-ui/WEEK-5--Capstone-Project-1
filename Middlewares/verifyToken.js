2bc5e536ef2b0b151ef26b1c20ee8104f3972663
23eg105d62-ui <23eg105d62@anurag.edu.in>
2026-05-21 22:29:18 +0530

    Add WEEK-5--CAPSTONE-PROJECT-1 project to Suntek_Mern_Assignments

--- Middlewares/verifyToken.js
+++ Middlewares/verifyToken.js
@@ -2,8 +2,9 @@ import jwt from "jsonwebtoken";
 import { config } from "dotenv";
 config();
 
-export const verifyToken = async (req, res, next) => {
-  
+export const verifyToken = (...allowedRoles)=>{
+  return async (req, res, next) => {
+  try{
     //read token from cookies 
     let token = req.cookies?.token ;
 
@@ -15,11 +16,26 @@ export const verifyToken = async (req, res, next) => {
     //verify the token (decoding the token)
     let decodedToken = jwt.verify(token, process.env.JWT_SECRET);
 
+    //check if role is allowed
+    if(!allowedRoles.includes(decodedToken.role)){
+      return res.status(403).json({message: "Forbidden. You don't have permission to access this resource"})
+    }
     //attach decoded token to request object for downstream use
     req.user = decodedToken;
 
     //forward req to next middleware/route
     next();
-  
+  }
+  catch(err){
+    //jwt.verify throws if token is invalid or expired
+    if(err.name==="TokenExpiredError" ){
+      return res.status(401).json({message: "Session expired. Please login again"})
+    }
+    if(err.name==="JsonWebTokenError" ){
+      return res.status(401).json({message: "Invalid token. Please login again"})
+    }
+    //next(err); //forward error to error handling middleware
+    }
 };
+}
 
